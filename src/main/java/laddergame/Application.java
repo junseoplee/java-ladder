523e72428551281304c4526d088661af67347f15
junseoplee <junseoplee@soongsil.ac.kr>
2024-02-02 06:39:40 +0900

    feat: 실행 어플리케이션 동작 구현

---
+++ src/main/java/laddergame/Application.java
@@ -0,0 +1,11 @@
+package laddergame;
+
+import laddergame.controller.LadderGameController;
+
+public class Application {
+
+  public static void main(String[] args) {
+    LadderGameController ladderGameController = new LadderGameController();
+    ladderGameController.runLadderGame();
+  }
+}
