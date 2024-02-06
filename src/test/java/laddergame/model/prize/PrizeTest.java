9695b5aae0254633afa7f11b26d2cef302bf996f
junseoplee <junseoplee@soongsil.ac.kr>
2024-02-06 23:24:02 +0900

    test: 결과(상금)에 공백이 포함되는 경우 예외 처리하는 기능 테스트

---
+++ src/test/java/laddergame/model/prize/PrizeTest.java
@@ -0,0 +1,15 @@
+package laddergame.model.prize;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PrizeTest {
+  @Test
+  @DisplayName("결과에_공백이_포함되면_예외_처리한다")
+  void 결과에_공백이_포함되면_예외_처리한다() {
+    assertThatThrownBy(() -> new Prize(" "))
+        .isInstanceOf(IllegalArgumentException.class);
+  }
+}
