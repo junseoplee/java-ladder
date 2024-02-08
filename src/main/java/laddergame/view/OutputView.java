458feaa9e3da53858937bd3a1eb3e77c6d3415df
junseoplee <junseoplee@soongsil.ac.kr>
2024-02-08 15:07:15 +0900

    feat: 사다리와 함께 실행 결과(상금)을 출력하는 메서드 구현

--- src/main/java/laddergame/view/OutputView.java
+++ src/main/java/laddergame/view/OutputView.java
@@ -4,16 +4,26 @@ import java.util.List;
 import laddergame.model.ladder.Ladder;
 import laddergame.model.participant.Participant;
 import laddergame.model.participant.Participants;
+import laddergame.model.prize.Prize;
+import laddergame.model.prize.Prizes;
 
 public class OutputView {
 
-  public void printResult(Participants participants, Ladder ladder) {
+  public void printLadder(Participants participants, Ladder ladder, Prizes prizes) {
     System.out.println("\n실행결과\n");
-    printParticipantsName(participants.getParticipants().stream()
-        .map(Participant::getParticipantName)
-        .toList());
-
+    printParticipantsName(
+        participants.getParticipants()
+                    .stream()
+                    .map(Participant::getParticipantName)
+                    .toList()
+    );
     ladder.getRungs().forEach(rung -> printRung(rung.getRungPoints()));
+    printPrizes(
+        prizes.getPrizes()
+              .stream()
+              .map(Prize::getPrize)
+              .toList()
+    );
   }
 
   private void printParticipantsName(List<String> participantNames) {
@@ -29,5 +39,10 @@ public class OutputView {
           LadderStandard.RUNG_EMPTY.getElement());
       System.out.print(LadderStandard.HEIGHT.getElement());
     }
+  }
+
+  private void printPrizes(List<String> prizes) {
+    System.out.println();
+    prizes.forEach(index -> System.out.printf("%-6s", index));
   }
 }
